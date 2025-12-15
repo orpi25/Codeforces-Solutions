5452723de0aea968e92c999ae290826df2f974cc
orpi25 <ruzela.orpi@gmail.com>
2025-12-16 00:23:16 +0600

    Add files via upload

---
+++ A.Buttons.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/problemset/problem/1858/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+    long long a, b, c;
+    cin >> a >> b >> c;
+
+    if (a > b) {
+    cout << "First\n";
+        }
+    else if (a < b) {
+    cout << "Second\n";
+        }
+    else {
+    if (c % 2 == 1)
+    cout << "First\n";
+    else
+cout << "Second\n";}
+}
+return 0;
+}
+
+
